db24860a4f535df102a0a3ee6cdb751c667fc824
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-12-02 10:41:50 +0200

    Update start_webserver.bat

--- start_webserver.bat
+++ start_webserver.bat
@@ -6,7 +6,7 @@ REM Näytä koneen paikallinen IP-osoite (esim. 192.168.1.123)
 echo Your local IP addresses:
 ipconfig | findstr /i "IPv4"
 
-REM HTTP-palvelin portissa 8008
+REM HTTP-palvelin 8008
 echo Starting Python HTTP server at port 8008...
 python -m http.server 8008 --bind 0.0.0.0
 
